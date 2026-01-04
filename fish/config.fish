f368514fe4e7755ec767e57f05a077da7dbb6563
marciosobel <marciosobel@proton.me>
2026-01-04 15:30:25 -0300

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,9 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    fastfetch
     # Remove welcome message
     set fish_greeting
-end
 
-set -gx PATH $PATH "/home/marci/.julia/bin"
+    # Commands to run in interactive sessions can go here
+end
 
 eval (ssh-agent -c) >/dev/null
 
@@ -18,8 +16,10 @@ set -gx PNPM_HOME "/home/marci/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
-
 set -gx ANDROID_SDK_ROOT $HOME/Android/Sdk
+
+set -gx PATH "$HOME/go/bin" $PATH
+set -gx PATH "$HOME/.config/fish/scripts" $PATH
 
 # env
 set -gx EDITOR helix
@@ -27,3 +27,34 @@ set -gx EDITOR helix
 # aliases
 alias vim=nvim
 alias hx=helix
+
+alias l='lsd -1A --group-directories-first --color=always'
+alias ls='lsd'
+alias la='lsd -l --permission=octal'
+alias tree='lsd --tree'
+
+alias ga='git add'
+alias gl='git log --graph --all --pretty=format:"%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n"'
+alias gs='git status --short'
+alias gc='git commit'
+alias gcm='git commit --message'
+alias gc!='git commit --amend'
+alias gc!!='git commit --amend --no-edit'
+alias gp='git push'
+alias gp!='git push --force'
+
+alias clock='tty-clock -ct -C 5'
+
+alias kubectl='minikube kubectl --'
+
+alias icat='kitty icat'
+
+alias paci='sudo pacman -S' # Install
+alias pacr='sudo pacman -Rs' # Remove
+alias pacs='pacman -Ss' # Search
+alias paclo='pacman -Qdt' # List orphans
+alias pacro='sudo pacman -Rns $(pacman -Qtdq)' # Remove orphans
+alias pacc='pacman -Scc' # Clean cache
+alias paclf='pacman -Ql' # List files
+alias yayi='yay -S --needed' # Install
+alias yayr='yay -R' # Remove
