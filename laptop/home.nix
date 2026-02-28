f769b190e29f5c9ad19bfc560e75a78fea1b9d0d
Márcio Sobel <marciosobel@proton.me>
2026-02-28 15:31:51 -0300

    add more programs

--- laptop/home.nix
+++ laptop/home.nix
@@ -8,6 +8,13 @@
     brightnessctl
     kitty
     fastfetch
+
+    krita
+    obsidian
+    mpv
+    yazi
+    dragon-drop
+    lazygit
   ];
 
   wayland.windowManager.hyprland.settings.monitor = [ "eDP-1, 1920x1080@60, 0x0, 1" ];
