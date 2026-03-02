11550b19660d09b7b0f5d6c8ec47236166028ef4
Márcio Sobel <marciosobel@proton.me>
2026-03-01 21:48:49 -0300

    enable `direnv`

--- modules/nixos/optimize.nix
+++ modules/nixos/optimize.nix
@@ -1,11 +1,12 @@
 {
+  nix.settings.auto-optimize-store = true;
   nix.gc = {
     automatic = true;
     dates = "15d";
     options = "--delete-older-than 15d";
   };
-  nix.settings.auto-optimize-store = true;
 
   zramSwap.enable = true;
   services.earlyoom.enable = true;
+  programs.direnv.enable = true;
 }
