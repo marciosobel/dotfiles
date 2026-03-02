f8f9172019f4112cff8b8eb2d22ff77ffdd392e3
Márcio Sobel <marciosobel@proton.me>
2026-03-01 21:48:17 -0300

    fix typo

--- modules/nixos/kernel.nix
+++ modules/nixos/kernel.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   boot = {
-    kernelPakcages = pkgs.linuxPackages_zen;
+    kernelPackages = pkgs.linuxPackages_zen;
     kernelParams = [
       # SWAP priority. Highest than 60 (default) is good for zram:
       # https://lists.ubuntu.com/archives/lubuntu-users/2013-October/005831.html
