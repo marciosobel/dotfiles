3a39a464e59be23395670dcf101a2fa18b19d6cb
Márcio Sobel <marciosobel@proton.me>
2026-03-04 20:35:56 -0300

    update git with more settings

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -8,9 +8,60 @@
         name = "Márcio Sobel";
         email = "marciosobel@proton.me";
       };
-      commit.gpgSign = true;
+
+      core = {
+        compression = 9;
+        whitespace = "error";
+        preloadindex = true;
+      };
+
+      status = {
+        branch = true;
+        showStash = true;
+        showUntrackedFiles = "all";
+      };
+
+      commit = {
+        gpgSign = true;
+        verbose = true;
+      };
+
+      log = {
+        abbrevCommit = true;
+        grapthColors = "blue,yellow,cyan,magenta,green,red";
+      };
+
+      pager = {
+        branch = false;
+        tag = false;
+      };
+
+      url."https://github.com/marciosobel/".insteadOf = "me:";
+      url."https://github.com/".insteadOf = "gh:";
+
+      interactive.singlekey = true;
+      push.autoSetupRemote = true;
+      branch.sort = "-commiterdate";
+      tag.sort = "-taggerdate";
+      init.defaultBranch = "dev";
       credential.helper = "manager";
       credential.credentialStore = "secretservice";
+
+      color = {
+        branch = {
+          current = "magenta";
+          local = "default";
+          remote = "yellow";
+          upstream = "green";
+          plain = "blue";
+        };
+        decorate = {
+          HEAD = "red";
+          branch = "blue";
+          tag = "yellow";
+          remoteBranch = "magenta";
+        };
+      };
     };
   };
 }
