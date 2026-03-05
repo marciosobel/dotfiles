6de24f5fa77e2d7b10adf299503edaa41b4245b5
Márcio Sobel <marciosobel@proton.me>
2026-03-05 00:32:56 -0300

    add rust alternatives to unix commands

--- laptop/home.nix
+++ laptop/home.nix
@@ -20,6 +20,12 @@
 
     # :3
     fastfetch
+
+    # rust alternatives to unix commands
+    lsd
+    bat
+    fzf
+    ripgrep
   ];
 
   wayland.windowManager.hyprland.settings.monitor = ["eDP-1, 1920x1080@60, 0x0, 1"];
