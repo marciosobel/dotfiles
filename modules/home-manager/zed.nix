8049869366821e52bba7b69254c530b7328072d8
Márcio Sobel <marciosobel@proton.me>
2026-03-09 08:09:21 -0300

    add zed settings

--- modules/home-manager/zed.nix
+++ modules/home-manager/zed.nix
@@ -11,5 +11,25 @@
       "proto"
       "discord-presence"
     ];
+
+    userSettings = {
+      helix_mode = true;
+      inline_code_actions = true;
+      which_key.enabled = true;
+      editpredictions.mode = "subtle";
+      calls.mute_on_join = true;
+      git = {
+        branch_picker.show_author_name = true;
+        inline_blame.show_commit_summary = true;
+      };
+      collaboration_panel.dock = "right";
+      gutter.min_line_number_digits = 2;
+      diagnostics.inline.enabled = true;
+      relative_line_numbers = "enabled";
+      terminal = {
+        shell.program = "fish";
+        toolbar.breadcrumbs = false;
+      };
+    };
   };
 }
