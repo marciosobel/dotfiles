86ffba65b5fbb2e51f0da613323a34e2f8238017
Márcio Sobel <marciosobel@proton.me>
2026-03-28 09:10:01 -0300

    simplify zen-browser config

--- modules/hm/zen-browser.nix
+++ modules/hm/zen-browser.nix
@@ -3,6 +3,8 @@
   system,
   ...
 }: let
+  zen-browser = inputs.zen-browser.packages.${system}.beta;
+
   mkExtensionSettings = builtins.mapAttrs (_: pluginId: {
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
     installation_mode = "force_installed";
@@ -124,30 +126,24 @@ in {
   };
 
   xdg.mimeApps = let
-    value = let
-      zen-browser = inputs.zen-browser.packages.${system}.beta;
-    in
-      zen-browser.meta.desktopFileName;
-
-    associations = builtins.listToAttrs (map (name: {
-        inherit name value;
-      }) [
-        "application/x-extension-shtml"
-        "application/x-extension-xhtml"
-        "application/x-extension-html"
-        "application/x-extension-xht"
-        "application/x-extension-htm"
-        "x-scheme-handler/unknown"
-        "x-scheme-handler/mailto"
-        "x-scheme-handler/chrome"
-        "x-scheme-handler/about"
-        "x-scheme-handler/https"
-        "x-scheme-handler/http"
-        "application/xhtml+xml"
-        "application/json"
-        "text/plain"
-        "text/html"
-      ]);
+    value = zen-browser.meta.desktopFileName;
+    associations = builtins.listToAttrs (map (name: {inherit name value;}) [
+      "application/x-extension-shtml"
+      "application/x-extension-xhtml"
+      "application/x-extension-html"
+      "application/x-extension-xht"
+      "application/x-extension-htm"
+      "x-scheme-handler/unknown"
+      "x-scheme-handler/mailto"
+      "x-scheme-handler/chrome"
+      "x-scheme-handler/about"
+      "x-scheme-handler/https"
+      "x-scheme-handler/http"
+      "application/xhtml+xml"
+      "application/json"
+      "text/plain"
+      "text/html"
+    ]);
   in {
     associations.added = associations;
     defaultApplications = associations;
