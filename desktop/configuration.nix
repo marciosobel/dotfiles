950b53c4e43a8fe04e12a19cc30bb267fe599ef2
Márcio Sobel <marciosobel@proton.me>
2026-03-29 09:26:50 -0300

    add split keyboard support

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -1,4 +1,12 @@
 {
+  pkgs,
+  user,
+  ...
+}: {
   imports = [./hardware-configuration.nix];
   services.xserver.xkb.layout = "us"; # Configure keymap in X11
+
+  environment.systemPackages = with pkgs; [vial];
+  services.udev.packages = with pkgs; [vial];
+  users.users.${user}.extraGroups = ["users"];
 }
