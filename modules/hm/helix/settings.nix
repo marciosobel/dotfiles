5bb92febfa8f30b6654b30ee9f67cff8c47ed8ab
Márcio Sobel <marciosobel@proton.me>
2026-04-05 15:22:43 -0300

    helix: add yazi integration

--- modules/hm/helix/settings.nix
+++ modules/hm/helix/settings.nix
@@ -21,6 +21,17 @@
       X = "select_line_above";
       space.x = ":bc";
       space.X = ":bc!";
+      space.e = let
+        unique-file = "/tmp/unique-file";
+      in [
+        ":sh rm -f ${unique-file}"
+        ":insert-output yazi \"%{buffer_name}\" --chooser-file=${unique-file}"
+        ":sh printf \"\\x1b[?1049h\\x1b[?2004h\" > /dev/tty"
+        ":open %sh{cat ${unique-file}}"
+        ":redraw"
+        ":set-option mouse false"
+        ":set-option mouse true"
+      ];
     };
     select = {
       x = "select_line_below";
