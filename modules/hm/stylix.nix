5075be0de2ac543fcc25847a09bd368a460c689f
Márcio Sobel <marciosobel@proton.me>
2026-05-07 12:52:40 -0300

    stylix: change to light mode

--- modules/hm/stylix.nix
+++ modules/hm/stylix.nix
@@ -3,29 +3,38 @@
 in {
   stylix = rec {
     enable = true;
-    base16Scheme = getTheme "gruvbox-material-dark-medium";
-    polarity = "dark";
+
+    base16Scheme = getTheme "gruvbox-material-light-medium";
+    polarity = "light";
+
     icons = {
       enable = true;
       package = pkgs.adwaita-icon-theme;
       dark = "Adwaita";
       light = "Adwaita";
     };
+
     cursor = {
       package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Classic";
+      name = "Bibata-Modern-Ice";
       size = 24;
     };
+
     fonts = {
+      sizes = {
+        terminal = 14;
+        popups = 12;
+      };
       sansSerif = {
         package = pkgs.texlivePackages.nunito;
         name = "Nunito";
       };
       monospace = {
-        package = pkgs.nerd-fonts.jetbrains-mono;
-        name = "JetBrainsMono Nerd Font";
+        package = pkgs.nerd-fonts.iosevka;
+        name = "Iosevka Nerd Font";
       };
     };
+
     targets = {
       waybar = {
         font = "sansSerif";
@@ -34,8 +43,7 @@ in {
       };
       kitty.opacity.override.terminal = 0.95;
       ghostty.opacity.override.terminal = 0.95;
-      wofi.fonts.override.monospace.name = "Nunito";
-      cava.rainbow.enable = true;
+      wofi.fonts.override.monospace.name = fonts.sansSerif.name;
       obsidian.fonts.override.sizes.applications = 16;
       helix.opacity.override.terminal = 0;
     };
