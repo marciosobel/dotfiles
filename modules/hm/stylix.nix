a0f8f0edc3ed6cf7a80b0561502711ab687a5cf5
Márcio Sobel <marciosobel@proton.me>
2026-05-08 13:49:49 -0300

    stylix: use `everforest-light-medium` theme

--- modules/hm/stylix.nix
+++ modules/hm/stylix.nix
@@ -4,7 +4,26 @@ in {
   stylix = rec {
     enable = true;
 
-    base16Scheme = getTheme "gruvbox-material-light-medium";
+    # while https://github.com/tinted-theming/schemes/pull/98 doesn't get merged in nixpkgs
+    base16Scheme = {
+      base00 = "#fdf6e3";
+      base01 = "#f4f0d9";
+      base02 = "#e6e2cc";
+      base03 = "#939f91";
+      base04 = "#829181";
+      base05 = "#5c6a72";
+      base06 = "#475258";
+      base07 = "#2d353b";
+      base08 = "#f85552";
+      base09 = "#f57d26";
+      base0A = "#dfa000";
+      base0B = "#8da101";
+      base0C = "#35a77c";
+      base0D = "#3a94c5";
+      base0E = "#df69ba";
+      base0F = "#829181";
+    };
+
     polarity = "light";
 
     icons = {
