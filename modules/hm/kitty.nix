26ab479597d2c5c56354f2c2908b4ea0f3868612
Márcio Sobel <marciosobel@proton.me>
2026-05-19 08:25:34 -0300

    kitty: fix shell integration and remove cursor blink

--- modules/hm/kitty.nix
+++ modules/hm/kitty.nix
@@ -11,12 +11,17 @@
       cursor_trail = 0;
       cursor_trail_decay = "0.05 0.4";
       cursor_trail_start_threshold = 0;
-      shell_integration = "no-cursor";
+      cursor_blink_interval = 0;
 
       remember_window_size = true;
       resize_in_steps = true;
 
       shell = "${pkgs.fish}/bin/fish";
+    };
+
+    shellIntegration = {
+      enableFishIntegration = true;
+      mode = "no-cursor";
     };
   };
 }
