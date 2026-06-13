f1a2a150e7e4d3e8b0d9e8cdc0dad5534bb53827
Márcio Sobel <marciosobel@proton.me>
2026-06-13 05:55:03 -0300

    obsidian: configure `corePlugins`

--- modules/hm/obsidian.nix
+++ modules/hm/obsidian.nix
@@ -7,6 +7,36 @@
         zoomLevel = 120;
         showRibbon = false;
       };
+
+      corePlugins = [
+        "file-explorer"
+        "global-search"
+        "switcher"
+        "graph"
+        "backlink"
+        "canvas"
+        "outgoing-link"
+        "tag-pane"
+        "properties"
+        "page-preview"
+        "daily-notes"
+        "templates"
+        "note-composer"
+        "command-palette"
+        "editor-status"
+        "bookmarks"
+        "outline"
+        "word-count"
+        "file-recovery"
+        "bases"
+        "webviewer"
+        {
+          name = "templates";
+          settings = {
+            folder = "templates";
+          };
+        }
+      ];
     };
   };
 }
