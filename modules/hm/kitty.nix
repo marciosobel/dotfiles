495b430a666781852f3f9ac00e1d70b949483c34
Márcio Sobel <marciosobel@proton.me>
2026-06-19 05:16:01 -0300

    kitty: add cursor trail

--- modules/hm/kitty.nix
+++ modules/hm/kitty.nix
@@ -8,7 +8,7 @@
       cursor_shape = "block";
       cursor_shape_unfocused = "hollow";
       cursor_beam_thickness = 2;
-      cursor_trail = 0;
+      cursor_trail = 1;
       cursor_trail_decay = "0.05 0.4";
       cursor_trail_start_threshold = 0;
       cursor_blink_interval = 0;
